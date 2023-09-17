501ce9508eb434fde069891a0ee0af8707341914
sanghyeok1000 <parksang10000@naver.com>
2023-09-17 21:36:17 +0900

    save 09/17-s

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
